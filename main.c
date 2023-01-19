be933ae4bc2b085684a900c961c057624cbc47d2
Omer Nachshon <Omer.Nachshon@s.afeka.ac.il>
2023-01-19 12:15:05 +0200

    add files

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+// This function performs heavy computations
+// No Changes to this function are allowed
+double heavy(double data, int loopSize)
+{
+    double sum = 0;
+    for (int i = 0; i < loopSize; i++)
+        sum += cos(exp(sin(data * (i % 11)))) / loopSize;
+    return sum;
+}
+// Sequential code to be parallelized
+int main(int argc, char *argv[])
+{
+    clock_t begin = clock();
+    int i;
+    int size = 100000;
+    int loopSize = 100000;
+    double *arr = (double *)malloc(size * sizeof(double));
+    for (i = 0; i < size; i++)
+        arr[i] = rand() / RAND_MAX;
+    double answer = 0;
+    for (i = 0; i < size; i++)
+        answer += heavy(arr[i], loopSize);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("answer = %e\n , time spent:%lf", answer, time_spent);
+}
---
+++ openmp.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+//  This function performs heavy computations
+//  No Changes to this function are allowed
+double heavy(double data, int loopSize)
+{
+    double sum = 0;
+    for (int i = 0; i < loopSize; i++)
+        sum += cos(exp(sin(data * (i % 11)))) / loopSize;
+    return sum;
+}
+// Sequential code to be parallelized
+int main(int argc, char *argv[])
+{
+    clock_t begin = clock();
+    int i;
+    int size = 1000;
+    int loopSize = 1000;
+    double *arr = (double *)malloc(size * sizeof(double));
+
+    // no need to use shared memory because each index is used once
+#pragma omp parallel for default(none)
+    for (i = 0; i < size; i++)
+        arr[i] = rand() / RAND_MAX;
+    double answer = 0;
+
+    // using +sum reduction should cover the addition
+    //  no write to array so shared memory is not required
+#pragma omp parallel for reduction(+ \
+                                   : sum)
+    for (i = 0; i < size; i++)
+        answer += heavy(arr[i], loopSize);
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("answer = %e\n , time spent:%lf", answer, time_spent);
+}
